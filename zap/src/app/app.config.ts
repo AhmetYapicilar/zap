cc6d2ca913c3fe7d18a4ef50d2b485b2e60df795
Ahmet Yapicilar <a.yapicilar96@gmail.com>
2024-10-21 16:26:40 +0200

    added pwa

--- zap/src/app/app.config.ts
+++ zap/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -6,7 +6,11 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"zapgame","appId":"1:890434539045:web:4e2775d115a923685739cc","storageBucket":"zapgame.appspot.com","apiKey":"AIzaSyDRKgmCgJsFBgaJbHQuEqHYsl_acCIKepo","authDomain":"zapgame.firebaseapp.com","messagingSenderId":"890434539045"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase())]
+  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp({ "projectId": "zapgame", "appId": "1:890434539045:web:4e2775d115a923685739cc", "storageBucket": "zapgame.appspot.com", "apiKey": "AIzaSyDRKgmCgJsFBgaJbHQuEqHYsl_acCIKepo", "authDomain": "zapgame.firebaseapp.com", "messagingSenderId": "890434539045" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })]
 };
