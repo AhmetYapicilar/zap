69c8c2f922cce2b882a654b25eb776f9bd44ee5b
BekirLajani <business.life.bekir@gmail.com>
2024-10-21 23:46:47 +0200

    button start funktion

--- zap/src/app/app.routes.ts
+++ zap/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { StartscreenComponent } from './startscreen/startscreen.component';
+import { GamePageComponent } from './game-page/game-page.component';
 
 export const routes: Routes = [
     { path: '', component: StartscreenComponent },
+    { path: 'game', component: GamePageComponent },
 ];
