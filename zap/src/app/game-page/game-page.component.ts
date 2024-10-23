0f34cfa7f41bc6ccd9459f41a70c90063db6290f
BekirLajani <business.life.bekir@gmail.com>
2024-10-23 17:12:30 +0200

    game-page

--- zap/src/app/game-page/game-page.component.ts
+++ zap/src/app/game-page/game-page.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { PlayersComponent } from '../players/players.component';
+
 
 @Component({
   selector: 'app-game-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,PlayersComponent],
   templateUrl: './game-page.component.html',
   styleUrl: './game-page.component.scss'
 })
 export class GamePageComponent {
 
+  handOut() {
+    console.log('test');
+    
+  }
 }
