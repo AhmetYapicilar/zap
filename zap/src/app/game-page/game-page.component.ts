fff57c8f815eb399c153230757853726803354ec
BekirLajani <business.life.bekir@gmail.com>
2024-10-23 17:28:25 +0200

    hand-out-card

--- zap/src/app/game-page/game-page.component.ts
+++ zap/src/app/game-page/game-page.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { PlayersComponent } from '../players/players.component';
+import { Game } from '../models/game';
 
 
 @Component({
@@ -11,9 +12,31 @@ import { PlayersComponent } from '../players/players.component';
   styleUrl: './game-page.component.scss'
 })
 export class GamePageComponent {
+  game: Game;
+  playerHands: string[][] = [];
 
-  handOut() {
-    console.log('test');
-    
+  constructor() {
+    this.game = new Game();
+    this.game.players = ['Justin', 'Sarah', 'Alex']; // Spieler hinzufügen
+    this.playerHands = this.game.players.map(() => []); // Spielerhände initialisieren
   }
+  handOut() {
+    const numberOfCards = 7; // Anzahl der Karten pro Spieler
+  
+    this.game.players.forEach((player, index) => {
+      for (let j = 0; j < numberOfCards; j++) {
+        const card = this.game.stack.pop();
+        if (card) { // Prüft, ob card nicht undefined ist
+          this.playerHands[index].push(card); // Karten an den Spieler austeilen
+          console.log(this.game.stack);
+          
+        } else {
+          console.error('Keine Karten mehr im Stapel!');
+        }
+      }
+    });
+    console.log('Karten ausgeteilt:', this.playerHands);
+  }
+  
 }
+
