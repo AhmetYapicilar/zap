a7ae176e0c4dab35318ddfb890ce4408b1295076
Ahmet Yapicilar <a.yapicilar96@gmail.com>
2024-10-24 17:56:33 +0200

    failed to show amount of cards

--- zap/src/app/players/players.component.ts
+++ zap/src/app/players/players.component.ts
@@ -1,23 +1,22 @@
-import { Component, OnInit ,Input, input } from '@angular/core';
+import { Component, OnInit, Input, input } from '@angular/core';
 import { Game } from '../models/game';
 import { GamePageComponent } from '../game-page/game-page.component';
+import { GameService } from '../game.service';
 
 @Component({
   selector: 'app-players',
   standalone: true,
   imports: [GamePageComponent],
   templateUrl: './players.component.html',
-  styleUrl: './players.component.scss'
+  styleUrl: './players.component.scss',
 })
 export class PlayersComponent {
   game: Game;
-  @Input() name:string = '';
-  constructor() {
+  cardAmount: number = 0;
+  @Input() name: string = '';
+  constructor(private gameService: GameService) {
     this.game = new Game();
     this.game.playerHands = {};
   }
-
-  
-
 
 }
