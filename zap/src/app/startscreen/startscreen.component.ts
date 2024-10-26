0d1aa547a8b019e6945c4560b63e7e0efb0b699f
Ahmet Yapicilar <a.yapicilar96@gmail.com>
2024-10-26 17:49:20 +0200

    add new Player

--- zap/src/app/startscreen/startscreen.component.ts
+++ zap/src/app/startscreen/startscreen.component.ts
@@ -3,18 +3,20 @@ import { Game } from '../models/game'
 import { GamePageComponent } from '../game-page/game-page.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { DialogComponent } from '../dialog/dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-startscreen',
   standalone: true,
-  imports: [GamePageComponent,CommonModule,FormsModule],
+  imports: [GamePageComponent,CommonModule,FormsModule, DialogComponent],
   templateUrl: './startscreen.component.html',
   styleUrl: './startscreen.component.scss'
 })
 export class StartscreenComponent implements AfterViewInit {
   name: string = '';
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, public dialog: MatDialog) {}
 
   ngAfterViewInit() {
     // Starte die Animation erst, wenn die Ansicht vollständig geladen ist
@@ -32,5 +34,16 @@ export class StartscreenComponent implements AfterViewInit {
 
   startNewGame() {
     this.router.navigateByUrl('/game/');
+    this.openDialog();
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        console.log('Player Name:', result); // Verwende den Namen hier weiter
+      }
+    });
   }
 }
